51794fb5782cb3231510f9c92c284d2835867c2b
John Pohl <johnpohl34@gmail.com>
2022-05-01 01:03:31 -0400

    components

--- app.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./src/components/PortfolioContainer";
-
-const app = () => <PortfolioContainer />;
-
-export default app;
--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-React.DOM.render(<app />, document.getElementById("root"));
