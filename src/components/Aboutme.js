da0807b2fd5dfc393f2e291f16200a08b45bcd3a
John Pohl <johnpohl34@gmail.com>
2022-05-01 20:17:27 -0400

    image folder

---
+++ src/components/Aboutme.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProfilePic from '../images/IMG_5394.jpg'
+
+function Aboutme() {
+    return (
+        <div>
+            <div class="container> 
+            <div class="row>
+                
+            </div>
+        </div>
+    )
+}
